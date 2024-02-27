ec2c3951f2c7c47075645c6b0f10ac28e7af693b
Manish <Manish989927.com>
2024-02-27 21:12:53 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(
+    () => {
+        console.log('Connected to MongoDB');
+    }
+).catch(
+    (err) => {
+        console.log('Failed to connect to MongoDB', err);
+    }
+);
+
+
+
+
 const app = express();
 
 app.listen(3000, () => {
