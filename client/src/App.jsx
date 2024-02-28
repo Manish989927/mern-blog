24f464bda07acbf4621cc962e4c7a991568f54f8
Manish <Manish989927.com>
2024-02-28 12:19:30 +0530

    create footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import SignUp from "./pages/SignUp"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/projects" element={<Projects />} />
     </Routes>
+    <Footer/>
     </BrowserRouter>
   )
 }
