c0eafef99cea2b9fd7c5feb038daf26381f381b9
Manish <Manish989927.com>
2024-03-01 22:26:45 +0530

    add redux persist

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -37,7 +37,7 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      return next(errorHandler(404, "User not found"));
+      returnnext(errorHandler(404, "User not found"));
     }
 
     const validPassword = bcryptjs.compareSync(password, validUser.password);
