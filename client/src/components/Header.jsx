75597ca31348aa65230d117defcf9e4a0acf0ea0
Manish <Manish989927.com>
2024-03-08 16:14:56 +0530

    complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { Navbar, TextInput, Button, Dropdown, Avatar } from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
 import React from "react";
-import { Link,useLocation } from "react-router-dom";
-import { FaMoon } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 export default function Header() {
-    const path = useLocation().pathname;
-    const {currentUser} = useSelector(state => state.user)
+  const path = useLocation().pathname;
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
   return (
     <Navbar className="border-b-2">
       <Link
@@ -34,56 +37,54 @@ export default function Header() {
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
-      
+
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon />
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === "light" ? <FaSun /> : <FaMoon />}
         </Button>
         {currentUser ? (
           <Dropdown
-          arrowIcon = {false}
-          inline
-          label = {<Avatar
-           alt="user"
-           img = {currentUser.profilePicture} rounded/>}>
-          <Dropdown.Header>
-            <div className="block-text-sm">@{currentUser.username}</div>
-            <div className="block-text-sm font-medium truncate">{currentUser.email}</div>
-          </Dropdown.Header>
-          <Link to={'/dashboard?tab=profile'}>
-            <Dropdown.Item>Profile</Dropdown.Item>
-          </Link>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <div className="block-text-sm">@{currentUser.username}</div>
+              <div className="block-text-sm font-medium truncate">
+                {currentUser.email}
+              </div>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
           </Dropdown>
-        ):
-        (
+        ) : (
           <Link to="/sign-in">
-          <Button gradientDuoTone='purpleToBlue'>
-            Sign In
-            </Button>
-        </Link>
-        )
-      }
+            <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
-        <Navbar.Collapse>
-            <Navbar.Link active={path==='/'} as={'div'}>
-                <Link to='/'>
-                    Home
-                </Link>
-            </Navbar.Link>
-            <Navbar.Link active={path==='/about'} as={'div'}>
-                <Link to='/about'>
-                    About
-                </Link>
-            </Navbar.Link>
-            <Navbar.Link active={path==='/projects'} as={'div'}>
-                <Link to='/projects'>
-                    Projects
-                </Link>
-            </Navbar.Link>
-        </Navbar.Collapse>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice'
 import {persistReducer} from 'redux-persist';
+import themeReducer from './theme/themeSlice';
 import storage from 'redux-persist/lib/storage';
 import persistStore from 'redux-persist/es/persistStore';
 
 const rootReducer = combineReducers({
     user:userReducer,
+    theme:themeReducer,
 });
 
 const persistConfig = {
